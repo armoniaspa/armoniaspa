5a87f99b01c361a0b7efa61dc977aeaf95c253e6
Daniel Ortiz <danielortiz179499@gmail.com>
2023-05-12 18:27:21 -0500

    Prueba

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<h1>Armonía Spa</h1>
+				<h1>Armonía Spa - 2</h1>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
